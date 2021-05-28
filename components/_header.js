ba524fc34c8dbe80e27eda22c698ac65e92a504f
brokenmold <jason@broken-mold.com>
2021-05-28 05:44:12 +0000

    Schafold Header Content

--- components/_header.js
+++ components/_header.js
@@ -7,8 +7,18 @@ export default function Header() {
   return(
     <header>
       <div className='header-container'>
-        <div>
-          <span className={style.title}>Domain Search</span>
+        <div className={style.wrapper}>
+
+          <div className={style.logo}>
+            <span className={style.title}>Domain Search</span>
+          </div>
+
+          <div className={style.nav}>
+            <a href='https://github.com/brokenmold/nextjs-domain-search' target='blank'>
+              <span className='fab fa-github fa-2xl'/>Get The Code
+            </a>
+          </div>
+
         </div>
       </div>
     </header>
