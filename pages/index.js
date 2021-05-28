e8f7a9de3e0198ad5fbc05d9e90bf25666396766
brokenmold <jason@broken-mold.com>
2021-05-28 21:18:46 +0000

    Add Search Form ~ Value to Console

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,30 @@ import Head from 'next/head'
 import style from '../styles/home.module.scss'
 
 export default function Home() {
+
+  const searchDomain = async event => {
+    event.preventDefault()
+
+    // const res = await fetch(
+    //   'https://hooks.zapier.com/hooks/catch/123456/abcde',
+    //   {
+    //     body: JSON.stringify({
+    //       name: event.target.name.value
+    //     }),
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     },
+    //     method: 'POST'
+    //   }
+    // )
+
+    // const result = await res.json()
+    // result.user => 'Ada Lovelace'
+
+    console.log(event.target.term.value)
+
+  }
+
   return (
     <div>
       <Head></Head>
@@ -17,6 +41,11 @@ export default function Home() {
         <div className={style.icon}>
           <div className='fad fa-sign fa-8x'/>
         </div>
+
+        <form onSubmit={searchDomain}>
+          <input id='term' type='text' autoComplete='term' required />
+          <button type='submit'>Search</button>
+        </form>
 
       </main>
     </div>
