0928f32fc91795cad7613aa62c4d3a17b3fd46cb
brokenmold <jason@broken-mold.com>
2021-05-28 17:56:54 +0000

    Add Copious Amount of Groovy Icons

--- pages/alt-icons.js
+++ pages/alt-icons.js
@@ -25,6 +25,40 @@ export default function Home() {
           <div className='fad fa-coffee-pot fa-8x'/>
           <div className='fad fa-industry fa-8x'/>
           <div className='fad fa-analytics fa-8x'/>
+          <div className='fad fa-search fa-8x'/>
+          <div className='fad fa-search-location fa-8x'/>
+          <div className='fad fa-raygun fa-8x'/>
+          <div className='fad fa-rocket-launch fa-8x'/>
+          <div className='fad fa-rocket fa-8x'/>
+          <div className='fad fa-sync fa-8x'/>
+          <div className='fad fa-sync-alt fa-8x'/>
+          <div className='fad fa-redo-alt fa-8x'/>
+          <div className='fad fa-recycle fa-8x'/>
+          <div className='fad fa-cog fa-8x'/>
+          <div className='fad fa-cogs fa-8x'/>
+          <div className='fad fa-brackets-curly fa-8x'/>
+          <div className='fad fa-smile-wink fa-8x'/>
+          <div className='fad fa-sad-tear fa-8x'/>
+          <div className='fad fa-dumpster-fire fa-8x'/>
+          <div className='fad fa-car-crash fa-8x'/>
+          <div className='fad fa-box-check fa-8x'/>
+          <div className='fad fa-box-open fa-8x'/>
+          <div className='fad fa-box-full fa-8x'/>
+          <div className='fad fa-glasses fa-8x'/>
+          <div className='fad fa-thumbs-down fa-8x'/>
+          <div className='fad fa-toggle-on fa-8x'/>
+          <div className='fad fa-trophy-alt fa-8x'/>
+          <div className='fad fa-user fa-8x'/>
+          <div className='fad fa-sliders-v-square fa-8x'/>
+          <div className='fad fa-share-square fa-8x'/>
+          <div className='fad fa-sign-in-alt fa-8x'/>
+          <div className='fad fa-home fa-8x'/>
+          <div className='fad fa-heart fa-8x'/>
+          <div className='fa fa-heart fa-8x'/>
+          <div className='far fa-heart fa-8x'/>
+          <div className='fad fa-satellite fa-8x'/>
+          <div className='fad fa-satellite-dish fa-8x'/>
+          <div className='fad fa-lightbulb-exclamation fa-8x'/>
         </div>
 
       </main>
