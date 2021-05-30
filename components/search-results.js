d65f6568f5ae750c55e680de1d28284bbacda6fa
brokenmold <jason@broken-mold.com>
2021-05-30 05:33:18 +0000

    Migrate Search Function

---
+++ components/search-results.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import Head from 'next/head'
+import axios from 'axios'
+
+import style from '../styles/home.module.scss'
+
+export default function Search() {
+
+  const searchDomain = async event => {
+    event.preventDefault()
+
+    const searchTerm = event.target.term.value.toLowerCase().replace(/\s+/g, '')
+
+    const reqDotDev = axios.request({
+      method: 'GET',
+      url: 'https://domainr.p.rapidapi.com/v2/status',
+      params: {
+        'mashape-key': process.env.RAPIDAPI_KEY,
+        domain: `${searchTerm}.dev`
+      },
+      headers: {
+        'x-rapidapi-key': process.env.RAPIDAPI_KEY,
+        'x-rapidapi-host': 'domainr.p.rapidapi.com'
+      }
+    })
+
+    const resDot = () => {
+      // *** Define Calls ***
+      axios.all([reqDotDev])
+      .then(axios.spread((...res) => {
+
+        for (let i = 0; i < res.length; i++) {
+          const resDot = res[i].data.status[0]
+
+          document.getElementById(`output-${resDot.zone}`).innerHTML = `
+            ${resDot.domain} ${ isAvailable(resDot.summary) } <br />
+          `
+        }
+      })).catch(errors => {
+        console.log(errors)
+      })
+    }
+
+    resDot()
+  }
+
+  // Check Domain Available
+  const isAvailable = (summary) => {
+    if (summary == 'inactive')
+      return '<span className="far fa-check"/> is availible!'
+    // else if (summary == 'active')
+    else
+      return '<i className"far fa-times"/> already taken'
+  }
+
+  return (
+    <div>
+      <form onSubmit={searchDomain}>
+        <input id='term' type='text' autoComplete='term' required />
+        <button type='submit'>Search</button>
+      </form>
+
+      <div>
+        <div id='output-dev'></div>
+      </div>
+    </div>
+  )
+}
