60eabf73d1c5a8d275bbfe3b1fc4f3553ac0636c
brokenmold <jason@broken-mold.com>
2021-06-03 01:11:29 +0000

    Refactor Sections & Column Size

--- components/_header.js
+++ components/_header.js
@@ -7,23 +7,23 @@ export default function Header() {
   return(
     <header>
       <div className='header-container'>
-        <div className={style.wrapper}>
+        <section className={style.wrapper}>
 
-          <div className={style.logo}>
+          <section className={style.logo}>
             <Link href='/'>
               <a>
                 <span className={style.title}>Domain Search</span>
               </a>
             </Link>
-          </div>
+          </section>
 
-          <div className={style.nav}>
+          <section className={style.nav}>
             <a className={style.nav} href='https://github.com/brokenmold/nextjs-domain-search' target='blank'>
               <span className='fab fa-github fa-2xl'/>Get The Code
             </a>
-          </div>
+          </section>
 
-        </div>
+        </section>
       </div>
     </header>
   )
