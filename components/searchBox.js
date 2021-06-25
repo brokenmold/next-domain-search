968cfacc13b5bd7e1f7f303692680b79728545e8
brokenmold <jason@broken-mold.com>
2021-06-25 17:53:15 +0000

    Cleanup

--- components/searchBox.js
+++ components/searchBox.js
@@ -1,20 +1,11 @@
 import React from 'react'
-import { useSearch } from './searchContext'
 import { useRouter } from 'next/router'
 import Random from 'project-name-generator'
 
-import styleSearchBox from '../styles/comp.searchBox.module.scss'
-import styleSearchBar from '../styles/comp.searchBar.module.scss'
-
-const style = styleSearchBox
-
-// console.log(searchStyle)
+import style from '../styles/comp.searchBox.module.scss'
 
 export default function SearchBox() {
-  const search = useSearch()
   const router = useRouter()
-
-  // console.log(router.locale)
 
   // onClick -- Search
   const searchDomain = async event => {
@@ -32,17 +23,11 @@ export default function SearchBox() {
     document.getElementById('term').value = `${rando}`
   }
 
-  const preventDefault = async event => {
-    event.preventDefault()
-  }
-
   return (
     <section className={style.wrapper}>
 
       <section className={style.search}>
         <div className={style.form}>
-
-          {search}
 
           <div className={style.item}>
             <input id='term' type='text' autoComplete='term' required />
