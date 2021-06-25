2f61ddb42df3dcbc882f98414910de3b5f5e5d2d
brokenmold <jason@broken-mold.com>
2021-06-25 19:04:48 +0000

    Migrate SASS to CSS

--- components/_header.js
+++ components/_header.js
@@ -1,26 +1,25 @@
 import React from 'react'
 import Link from 'next/link'
 
-import style from '../styles/layout.header.module.scss'
 
 export default function Header() {
   return(
     <header>
       <div className='header-container'>
-        <section className={style.wrapper}>
+        <section className='headerWrapper'>
 
-          <section className={style.logo}>
+          <section className='logo'>
             <Link href='/'>
               <a>
-                <span className={style.title}>Domain Search</span>
+                <span className='title'>Domain Search</span>
               </a>
             </Link>
           </section>
 
-          <section className={style.nav}>
-            <a className={style.nav} href='https://github.com/brokenmold/nextjs-domain-search' target='blank'>
+          <section className='nav'>
+            <a className='nav' href='https://github.com/brokenmold/nextjs-domain-search' target='blank'>
               <span className='fab fa-github fa-2xl'/>
-              <span className={style.navText}>Get The Code</span>
+              <span className='navText'>Get The Code</span>
             </a>
           </section>
 
