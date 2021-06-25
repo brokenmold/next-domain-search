982bf9b1908ea018903cfac2c7bccbdd7f122010
brokenmold <jason@broken-mold.com>
2021-06-25 18:19:31 +0000

    Migrate SASS to CSS

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,15 @@
 import Head from 'next/head'
 import SearchBox from '../components/searchBox'
 
-import style from '../styles/page.index.module.scss'
-
-const searchStyle = 'search-box'
-
 export default function Home() {
   return (
     <div>
       <Head></Head>
 
-      <section className={style.wrapper}>
+      <section className='homeWrapper'>
 
-        {/* HERO BOX */}
-        <section className={style.heroBox}>
-          <div className={style.icon}>
+        <section className='heroBox'>
+          <div className='icon'>
             <div className='fas fa-sign fa-8x'/>
           </div>
           <h1>Digital Real Estate</h1>
@@ -22,12 +17,10 @@ export default function Home() {
 
         </section>
 
-        {/* SEARCH BOX */}
-        <section className={style.searchBox}>
+        <section className='searchBox'>
           <SearchBox/>
         </section>
 
-        {/* AFFILIATE TAG */}
         {/* <div className={style.digitalOcean}>
           <a href='https://m.do.co/c/cb06502b2c00' target='_blank'><span class='fab fa-digital-ocean fa-2x'/> Powered by DigitalOcean</a>
         </div> */}
